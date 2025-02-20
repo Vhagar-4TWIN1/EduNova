ed71875391ff4d63cda11d7a958f7fff957294cb
YosrBenAmor <benamoryosr1407@gmail.com>
2025-02-20 12:15:53 +0100

    remember Acompte
    integration de plus de template avec login + remember me

--- src/App.jsx
+++ src/App.jsx
@@ -1,32 +1,24 @@
-import { useState } from 'react';
-import 'bootstrap/dist/css/bootstrap.min.css';
-import './assets/css/main.css';
-import './assets/vendor/bootstrap-icons/bootstrap-icons.css';
-import './assets/vendor/aos/aos.css';
-import './assets/vendor/glightbox/css/glightbox.min.css';
-import './assets/vendor/swiper/swiper-bundle.min.css';
-
-import heroBg from './assets/img/hero-bg.jpg';
-import aboutImg from './assets/img/about.jpg';
-import course1 from './assets/img/course-1.jpg';
-import course2 from './assets/img/course-2.jpg';
-import course3 from './assets/img/course-3.jpg';
-import trainer1 from './assets/img/trainers/trainer-1.jpg';
-import trainer2 from './assets/img/trainers/trainer-2.jpg';
-import trainer3 from './assets/img/trainers/trainer-3.jpg';
-import Footer from './components/footer';
-
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Login from "./components/login";
+import Registration from "./components/registration";
+import Home from "./components/home";
+import Layout from "./components/layout";
+import AOS from 'aos';
+import 'aos/dist/aos.css';
+AOS.init();
 function App() {
-  const [count, setCount] = useState(0);
-
   return (
-<div className="page-container">
-      <main className="content">
-        <img src={trainer1} alt="" />
-      </main>
-    <Footer />
-</div>
-    
+    <Router>
+      <Routes>
+        <Route path="/" element={<Login />} />
+        <Route path="/registration" element={<Registration />} />
+
+        {/* Routes avec Header et Footer */}
+        <Route element={<Layout />}>
+          <Route path="/home" element={<Home />} />
+        </Route>
+        </Routes>
+    </Router>
   );
 }
 
