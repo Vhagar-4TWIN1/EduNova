fc0f384135ddb48daf24f95d5b3778ec05ebdc36
louay abidi <louay.abidi@esprit.tn>
2025-02-24 15:15:08 +0100

    today's commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,20 @@
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Login from "./components/login";
 import Registration from "./components/registration";
+import Home from "./components/home";
+import Contact from "./components/Contact";
+import Message from "./components/messga";
 
 function App() {
   return (
     <Router>
       <Routes>
-        <Route path="/" element={<Login />} />
+        <Route path="/" element={<Home />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/contact" element={<Contact />} />
         <Route path="/registration" element={<Registration />} />
-        </Routes>
+        <Route path="/message" element={<Message />} />
+      </Routes>
     </Router>
   );
 }
