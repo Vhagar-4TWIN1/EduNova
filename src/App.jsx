c40ec4f48039e4763ecd8f642b36b01c0befc846
YosrBenAmor <benamoryosr1407@gmail.com>
2025-02-25 01:31:45 +0100

    Affichage de logs dans dashboard

--- src/App.jsx
+++ src/App.jsx
@@ -2,28 +2,34 @@ import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Login from "./components/login";
 import Registration from "./components/registration";
 import Home from "./components/home";
-import Logs from "./components/logs";
 import Layout from "./components/layout";
+import LinkedInCallback from "./components/linkedInCallback";
+import DashboardLayout from "./components/dashboard/dashboardLayout"; // Importez le DashboardLayout
+import Dashboard from "./components/dashboard/dashboard";
 import AOS from 'aos';
 import 'aos/dist/aos.css';
-import LinkedInCallback from "./components/linkedInCallback";
+
 AOS.init();
+
 function App() {
   return (
     <Router>
       <Routes>
+        {/* Routes publiques */}
         <Route path="/" element={<Login />} />
         <Route path="/registration" element={<Registration />} />
         <Route path="/auth/linkedin/callback" element={<LinkedInCallback />} />
 
-
-        {/* Routes avec Header et Footer */}
+        {/* Routes protégées avec Layout */}
         <Route element={<Layout />}>
           <Route path="/home" element={<Home />} />
-          <Route path="/logs" element={<Logs />} />
-
         </Route>
-        </Routes>
+
+        {/* Routes du tableau de bord */}
+        <Route element={<DashboardLayout />}>
+          <Route path="/dashboard" element={<Dashboard />} />
+        </Route>
+      </Routes>
     </Router>
   );
 }
