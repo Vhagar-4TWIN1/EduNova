cb413fa426ddb6ad08a6f55e52a084a8a5f7a6a1
oussamaawledsalem <oussama.awledsalem@esprit.tn>
2025-05-13 20:35:12 +0100

    fix dep

--- src/components/module/listModulesBack.jsx
+++ src/components/module/listModulesBack.jsx
@@ -1,8 +1,7 @@
 import React, { useEffect, useState } from "react";
 import { useNavigate } from "react-router-dom";
 import axios from "axios";
-import Header from "../Header";
-import Footer from "../Footer";
+
 import AddModule from "./addModule";
 import "./ListModules.css";
 import "bootstrap/dist/js/bootstrap.bundle.min";
